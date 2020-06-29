03823b81be34a1ab6422117511f360e48439a875
Sandeep Vaman Bende <sandeep@rapido.bike>
2020-06-29 15:54:36 +0530

    Added killer parameters to config

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -6,5 +6,11 @@ const SpotterNodeSelectors = "spotter.label_selectors"
 const SpotterPollIntervalMs = "spotter.poll_interval_ms"
 const SpotterExpiryTimeAnnotation = "silent-assassin/expiry-time"
 
+const KillerDeletionIntervalMs = "killer.deletion_interval"
+
+//Should be in ms or secs for uniformity??
+const KillerDrainingTimeoutSecs = "killer.draining_timeout"
+const KillerPollIntervalMs = "killer.poll_interval_ms"
+
 const LogComponentName = "SILENT_ASSASSIN"
 const LogLevel = "logger.level"
